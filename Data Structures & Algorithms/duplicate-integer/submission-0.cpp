2950377cc5fb7fa1f16301fbe547eb9e289ec27a
Shoryan Agarwal <shoryanagarwal154@gmail.com>
2026-09-17 01:35:52 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+            unordered_map<int,int> freq;
+
+            for(int i=0;i<nums.size();i++){
+                freq[nums[i]]++;
+
+                if(freq[nums[i]]>1){
+                    return true;
+                }
+            }
+
+            return false;
+
+            
+
+
+    }
+};
